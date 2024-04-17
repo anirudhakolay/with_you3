851a887e8c15f40e97e0e51a4dba29e99d105020
anirudha <anirudhakolay08062001@gmail.com>
2024-04-17 16:56:32 +0530

    origin main

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect("mongodb+srv://anirudha_kolay:admin@cluster0.8b2pbcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+		await mongoose.connect("mongodb+srv://anirudhakolay08062001:admin@cluster0.7qsorwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
