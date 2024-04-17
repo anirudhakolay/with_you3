41933256f53d0c9cca2c47d52ae89edd1f0813b4
anirudha <anirudhakolay08062001@gmail.com>
2024-04-17 18:05:24 +0530

    dfdfdrer

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
+/*app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
-
+*/
 
 
 app.get("/", (req, res) => {
